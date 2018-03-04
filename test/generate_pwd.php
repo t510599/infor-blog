ec10f732270f6a9cf81ff6f9284c671936a4b314
t510599 <t510599@gmail.com>
2018-03-04 16:30:04 +0800

    remove test files

--- test/generate_pwd.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('../include/function.php');
-
-if (isset($_POST['username'])&&isset($_POST['password'])) {
-    echo pwd($_POST['password'],$_POST['username']);
-} else {
-    echo "error";
-}
-?>
